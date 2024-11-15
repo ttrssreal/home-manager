2057687e8370472b23b84555971f9eee8a28f187
Jess <jess@jessie.cafe>
2024-11-16 07:25:39 +1300

    rclone: add module
    Rclone is a command-line program to manage files on cloud storage, it
    also featrues support for FUSE mounts.
    
    "Users call rclone *"The Swiss army knife of cloud storage"* and
    *"Technology indistinguishable from magic"*" - https://rclone.org/
    
    This module manages the configuration of rclone remotes.

--- modules/modules.nix
+++ modules/modules.nix
@@ -205,6 +205,7 @@ let
     ./programs/qutebrowser.nix
     ./programs/ranger.nix
     ./programs/rbw.nix
+    ./programs/rclone.nix
     ./programs/readline.nix
     ./programs/rio.nix
     ./programs/ripgrep.nix
